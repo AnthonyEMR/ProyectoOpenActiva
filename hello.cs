7d011b5453a18aaade07a54e619fcfdac0800380
Anthony <anthony.mayhuay@openactiva.com>
2022-10-06 03:32:49 +0000

    Hello World

---
+++ hello.cs
@@ -0,0 +1,14 @@
+using System;
+  
+namespace HelloWorldApp {
+    
+    class Geeks {
+          
+        // Main Method
+        static void Main(string[] args) {
+            
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
